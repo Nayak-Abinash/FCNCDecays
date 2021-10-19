5bf531ab9499a75a21c9f753311ba37456b7443c
Abinash Kumar Nayak <67491483+Nayak-Abinash@users.noreply.github.com>
2021-10-19 10:16:00 +0200

    Add files via upload

--- smpar.cc
+++ smpar.cc
@@ -1,18 +1,66 @@
-
 #include "smpar.h"
 #include <iostream>
 #include <cmath>
 #include <string>
-
 using namespace std;
 
-
-smpar :: smpar() {
-    //SM parameter values:
-    pi=M_PI;
-    mB=5.27953,mKst=0.896,mu=4.8,mc=1.27,mb=4.8;
-    C1=-0.257,C2=1.009,C3=-0.005,C4=-0.078,C5=0.000,C6=0.001,C7effRe=-0.304,C8eff=-0.167,C10effRe=-4.103;
-    
-    tauB=1.519*1.52*pow(10.0,12.0),alpha_e=1.0/133.0,GF1.16637*pow(10.0,-5.0),absVtbVtsStr=0.0409,me=0.511*pow(10.0,-3.0),mmu=105.658*pow(10.0,-3.0);
-}
+smpar::smpar(){pi=M_PI;}
+//Quark_Mass
+double smpar::md(){return 0.00467;}
+double smapr::mc(){return 1.27;}
+double smapr::ms(){return 0.093;}
+double smapr::mb(){return 4.18;}
+//Lepton_Mass
+double smapr::me(){return 0.511*pow(10.0,-3.0);}
+double smapr::mmu(){return 105.658*pow(10.0,-3.0);}
+double smapr::mtau(){return 1.77686;}
+//Meson_Mass
+double smapr::mBd(){return 5.27965;}
+double smapr::mBs(){return 5.36688;}
+double smapr::mK(){return 0.497614;}
+double smapr::mKst(){return 0.896;}
+//Width_Lifetime
+double smapr::tauBd(){return 1.519*1.52*pow(10.0,12.0);}
+double smapr::tauBs(){return 1.527*1.52*pow(10.0,12.0);}
+double smapr::DGamma_dbar(){return 0.001;}
+double smapr::DGamma_sbar(){return 0.124;}
+double smapr::fBd(){return 0.1905;}
+double smapr::fBs(){return 0.2303;}
+//Coupling_Constants
+double smapr::absVtbVtdStr(){return 0.00853871;}
+double smapr::absVtbVtsStr(){return 0.0408726;}
+double smapr::GF(){return 1.16637*pow(10.0,-5.0);}
+double smapr::alpha_e(){return 1.0/127.944;}
+//SM_Wilson_Coefficients
+double smapr::C1(){return -0.257;}
+double smapr::C2(){return 1.009;}
+double smapr::C3(){return -0.005;}
+double smapr::C4(){return -0.078;}
+double smapr::C5(){return 0.000;}
+double smapr::C6(){return 0.001;}
+double smapr::C7effRe(){return -0.304;}
+double smapr::C8eff(){return -0.167;}
+double smapr::C9Re(){return 4.211;}
+double smapr::C9Im(){return 0.0;}
+double smapr::C10Re(){return -4.103;}
+double smapr::C10Im(){return 0.0;}
+//NP_Wilson_Coefficients
+double smapr::C7RHRe(){return 0.0;}
+double smapr::C7RHIm(){return 0.0;}
+double smapr::C9RHRe(){return 0.0;}
+double smapr::C9RHIm(){return 0.0;}
+double smapr::C10RHRe(){return 0.0;}
+double smapr::C10RHIm(){return 0.0;}
+double smapr::CSRe(){return 0.0;}
+double smapr::CSIm(){return 0.0;}
+double smapr::CSRHRe(){return 0.0;}
+double smapr::CSRHIm(){return 0.0;}
+double smapr::CPRe(){return 0.0;}
+double smapr::CPIm(){return 0.0;}
+double smapr::CPRHRe(){return 0.0;}
+double smapr::CPRHIm(){return 0.0;}
+double smapr::CTRe(){return 0.0;}
+double smapr::CTIm(){return 0.0;}
+double smapr::CT5Re(){return 0.0;}
+double smapr::CT5Im(){return 0.0;}
 
--- smpar.h
+++ smpar.h
@@ -1,21 +1,21 @@
-
 #ifndef smpar_h
 #define smpar_h
 
 #include <iostream>
 #include <cmath>
 #include <string>
-
 using namespace std;
 
-//B->ll:
 class smpar {
 public:
-    double pi,mB,mKst,mu,mc,mb;
-    
-    double C1,C2,C3,C4,C5,C6,C7effRe,C8eff,C10effRe;
-    
-    double tauB,alpha_e,GF,absVtbVtsStr,me,mmu;
+    double pi;
+    double mb(),md(),ms();
+    double me(),mmu(),mtau();
+    double mBd(),mBs(),mKst();
+    double tauBd(),tauBs(),DGamma_dbar(),DGamma_sbar(),fBd(),fBs();
+    double absVtbVtdStr(),absVtbVtsStr(),GF(),alpha_e();
+    double C1(),C2(),C3(),C4(),C5(),C6(),C7effRe(),C8eff(),C10effRe(),C9Re(),C10Re(),C9Im();
+    double C9RHRe(),C9RHIm(),C10Im(),C10RHRe(),C10RHIm(),CSRe(),CSIm(),CSRHRe(),CSRHIm(),CPRe(),CPIm(),CPRHRe(),CPRHIm();
     
     smpar ();
 };
