fe57c0af5ede7fb4bf73d0e06da9a35e5bb323ef
Abinash Kumar Nayak <67491483+Nayak-Abinash@users.noreply.github.com>
2021-10-19 13:15:59 +0200

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -2,22 +2,20 @@
 #include<cmath>
 using namespace std;
 
-class par {
+class par{
 public:
-    double pi,mB,mKstr();
     par();
+    double md();
+private:
+    double mdv;
 };
 
-par :: par() {
-    pi=M_PI;
-    mB=5.27965;
-}
-double par :: mKstr() {
-    return 0.896;
-}
+par::par(){mdv=0.5;}
+
+double par::md(){return mdv;}
 
 int main(){
     par p;
-    cout << pow(p.mB-p.mKstr(),2.0) << endl;
+    cout << pow(p.md(),2.0) << endl;
     return 0;
 }
