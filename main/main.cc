4351c8da28fadffb1b570195167ee2de52552e72
Abinash Kumar Nayak <67491483+Nayak-Abinash@users.noreply.github.com>
2022-01-28 10:59:48 +0100

    Update main.cc

--- main/main.cc
+++ main/main.cc
@@ -33,31 +33,6 @@ int main(){
     sort(data,data+iter);
     cout << "Observable at this qsq: " << cval << "(+" << data[iter-sdcl-1]- cval << ",-" << cval-data[sdcl] << ")" << endl;
 
-////Histogram/////////////////////
-    /*double minv = *min_element(data,data+iter);
-    double maxv = *max_element(data,data+iter);
-    double nbin = 20;
-    double init_val = minv;
-    for (int i=0; i<nbin; ++i)
-        {
-            int npnt = mylength(data,iter,minv+(i)*(maxv-minv)/nbin,minv+(i+1)*(maxv-minv)/nbin);
-            if(i<10)
-                {
-                    cout << "-" << "bin-0" << i << ": ";
-                    cout << string(npnt/(iter/100),'*') << "\t \t" << npnt <<endl;
-                }
-            else
-                {
-                    cout << "-" << "bin-" << i << ": ";
-                    cout << string(npnt/(iter/100),'*') << "\t \t" << npnt <<endl;
-                }
-        }
-
-    cout << "(" << maxv << "," << minv << ")" << endl;
-////OutPut///////////////////////
-    cout << "The Observable value at this qsq:" << o1.AFB(qsq,p.mmu()) << endl;
-    cout << "The Mean and Standard Deviation at this qsq:" << "(" << meanv << "," << stdv <<")" << endl;*/
-
     return 0;
 }
 
