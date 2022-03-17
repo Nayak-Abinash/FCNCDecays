0a8443574081347c61d05c1302ae6b7d53124b09
Abinash Kumar Nayak <abinashkumarnayak@gmail.com>
2022-03-17 17:10:46 +0100

    March17th

---
+++ build/CMakeFiles/output.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/output.dir/main/main.cc.o"
+  "CMakeFiles/output.dir/src/FF_functions.cc.o"
+  "CMakeFiles/output.dir/src/FF_functions_mc.cc.o"
+  "CMakeFiles/output.dir/src/FF_parameters.cc.o"
+  "CMakeFiles/output.dir/src/FF_parameters_mc.cc.o"
+  "CMakeFiles/output.dir/src/SM_parameters.cc.o"
+  "CMakeFiles/output.dir/src/SM_parameters_mc.cc.o"
+  "CMakeFiles/output.dir/src/amplitudes.cc.o"
+  "CMakeFiles/output.dir/src/amplitudes_mc.cc.o"
+  "CMakeFiles/output.dir/src/ckm_elements.cc.o"
+  "CMakeFiles/output.dir/src/ckm_elements_mc.cc.o"
+  "CMakeFiles/output.dir/src/model_functions.cc.o"
+  "CMakeFiles/output.dir/src/observables.cc.o"
+  "CMakeFiles/output.dir/src/observables_mc.cc.o"
+  "output"
+  "output.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/output.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
